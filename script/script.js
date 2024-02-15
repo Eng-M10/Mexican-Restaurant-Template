a9345310b411c7e79368d11ca0d29196a70d19e3
Muvimbene Daniel Maposse <muvi10.com@gmail.com>
2024-02-15 19:36:17 +0200

    doc: CSS & JS = feat : Fix navigation bar and smooth scroll.

--- script/script.js
+++ script/script.js
@@ -1,8 +1,47 @@
 /**Hint
- * 1 - scrool smoth
- * 2 - Button entrance
- * 3 - Sounds
- * 4 - Navigation
- * 5 - Text Animation
+ * 1 - scrool smoth (X) [DONE]
+ * 2 - Button entrance ()
+ * 3 - Sounds ()
+ * 4 - Navigation (x) [DONE]
+ * 5 - Text Animation ()
  */
+
+function smoothScrollNavigation(){
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            const identifier = e.currentTarget.getAttribute('href');
+            e.preventDefault();
+            const target = document.querySelector(identifier);
+
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        });
+    });
+}
+
+
+function fixeMenuBar() {
+    
+    window.addEventListener('scroll', (e)=> {
+
+        const header = document.querySelector('header');
+        const currentScrool = window.scrollY;
+    
+        if(currentScrool >= 107) {
+            header.classList.add("fixedbar");
+        }
+        else {
+            header.classList.remove('fixedbar');
+        }
+    
+    });
+
+}
+
+console.clear()
+fixeMenuBar()
+smoothScrollNavigation()
 
